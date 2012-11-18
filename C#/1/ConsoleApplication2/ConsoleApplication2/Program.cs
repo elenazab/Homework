682b530b1427facda50b9b44658ed51439400456
Elena <zlena@list.ru>
2012-11-18 15:37:41 +0400

    Fibonacci number and array sorting in c#

---
+++ C#/1/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fibonacciNumber1 = 1;
+            int fibonacciNumber2 = 1;
+            int fibonacciNumber3 = 0;
+            for (int i = 3; i <= n; i++)
+            {
+                fibonacciNumber3 = fibonacciNumber1 + fibonacciNumber2;
+                fibonacciNumber1 = fibonacciNumber2;
+                fibonacciNumber2 = fibonacciNumber3;
+            }
+            Console.WriteLine(fibonacciNumber3);
+            Console.Read();
+        }
+    }
+}
---
+++ C#/1/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        static void bubbleSort(ref int[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+                for(int j = 0; j < n - 1; j++)
+                    if(a[j] > a[j+1])
+                        swap(ref a[j], ref a[j+1]);
+        }
+
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] myArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            bubbleSort(ref myArray, n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(myArray[i]);
+            }
+            Console.Read();
+        }
+    }
+}
