5a847b194a952d8a3860c16e9acc496c175fd8c2
Elena <zlena@list.ru>
2012-11-18 16:05:12 +0400

    ы

--- C#/1/ConsoleApplication3/ConsoleApplication3/Program.cs
+++ C#/1/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -4,30 +4,30 @@ namespace ConsoleApplication3
 {
     class Program
     {
-        static void swap(ref int a, ref int b)
+        static void Swap(ref int a, ref int b)
         {
             int c = a;
             a = b;
             b = c;
         }
 
-        static void bubbleSort(ref int[] a, int n)
+        static void BubbleSort(int[] a, int n)
         {
             for (int i = 0; i < n; i++)
                 for(int j = 0; j < n - 1; j++)
-                    if(a[j] > a[j+1])
-                        swap(ref a[j], ref a[j+1]);
+                    if(a[j] > a[j + 1])
+                        Swap(ref a[j], ref a[j + 1]);
         }
 
         static void Main()
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            int[] myArray = new int[n];
+            var myArray = new int[n];
             for (int i = 0; i < n; i++)
             {
                 myArray[i] = Convert.ToInt32(Console.ReadLine());
             }
-            bubbleSort(ref myArray, n);
+            BubbleSort(myArray, n);
             for (int i = 0; i < n; i++)
             {
                 Console.Write(myArray[i]);
