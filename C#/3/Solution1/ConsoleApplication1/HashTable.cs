967ba927664486fbac0a1be3ce616a366231d375
Elena <zlena@list.ru>
2012-11-19 00:00:01 +0400

    hash table

---
+++ C#/3/Solution1/ConsoleApplication1/HashTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace HashTable
+{
+    class HashTable
+    {
+        private int n;
+        private LinkedList<string>[] table;
+
+        public HashTable(int N)
+        {
+            n = N;
+            table = new LinkedList<string>[n];
+            for (int i = 0; i < n; i++)
+                table[i] = new LinkedList<string>();
+        }
+
+        private int Function(string s)
+        {
+            int result = 0;
+            int i = 0;
+            while (i < s.Length - 1)
+            {
+                result += s[i];
+                i++;
+            }
+            return result;
+        }
+
+        public void Add(string s)
+        {
+            int tmp = Function(s) % n;
+            table[tmp].AddFirst(s);
+        }
+
+        public void Del(string s)
+        {
+            int tmp = Function(s) % n;
+            if (table[tmp].Contains(s))
+                table[tmp].Remove(s);
+            else
+                Console.WriteLine("not found");
+        }
+
+        public void PrintTable()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                while (table[i].First != null)
+                {
+                    Console.WriteLine(table[i].First.Value);
+                    table[i].RemoveFirst();
+                }
+            }
+        }
+    }
+}
---
+++ C#/3/Solution1/ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main()
+        {
+             HashTable myTable = new HashTable(2);
+             myTable.Add("ololo");
+             myTable.Add("olololo");
+             myTable.PrintTable();
+             Console.Read();
+        }
+    }
+}
