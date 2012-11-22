037d2cfdf7d5e2a1dc71ec994dee3552cc6daad7
Elena <zlena@list.ru>
2012-11-23 02:18:25 +0400

    HashFunctions

---
+++ C#/3/Solution1/ConsoleApplication1/HashFunction1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HashTable
+{
+    public class HashFunction1 : HashFunction
+    {
+        public override int Function(string s)
+        {
+            int result = 0;
+            int i = 0;
+            while (i < s.Length - 1)
+            {
+                result += s[i];
+                i++;
+            }
+            return result;
+        }
+    }
+}
---
+++ C#/3/Solution1/ConsoleApplication1/HashFunction2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace HashTable
+{
+    public class HashFunction2 : HashFunction
+    {
+        public override int Function(string s)
+        {
+            int result = 0;
+            int i = 0;
+            while (i < s.Length - 1)
+            {
+                result += s[i] * s[i];
+                i++;
+            }
+            return result;
+        }
+    }
+}
