1853f2874bb2d9de1184913901241a56f1240cda
Elena <zlena@list.ru>
2012-11-27 21:10:41 +0400

    priority queue with exeptions

---
+++ C#/8/ConsoleApplication1/ConsoleApplication1/PriorityQueue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class PriorityQueue<T>
+    {
+        private class ListElement
+        {
+            public ListElement()
+            { }
+
+            public ListElement (T v, int p)
+            {
+                next = null;
+                value = v;
+                prio = p;
+            }
+
+            public ListElement GetNext()
+            {
+                return next;
+            }
+
+            public void SetNext(ListElement nxt)
+            {
+                next = nxt;
+            }
+
+            public T GetValue()
+            {
+                return value;
+            }
+
+            public int GetPrio()
+            {
+                return prio;
+            }
+
+            private T value;
+            private ListElement next;
+            private int prio;
+        }
+
+        public PriorityQueue()
+        {
+            head = new ListElement();
+        }
+
+        public void Enqueue(T val, int prio)
+        {
+            ListElement l = head;
+            while (l.GetNext() != null && l.GetNext().GetPrio() > prio)
+            {
+                l = l.GetNext();
+            }
+            ListElement tmp = new ListElement(val, prio);
+            tmp.SetNext(l.GetNext());
+            l.SetNext(tmp);
+        }
+
+        public T Dequeue()
+        {
+            if (head.GetNext() == null)
+                throw new QueueExeption();
+            ListElement tmp = head.GetNext();
+            head.SetNext(tmp.GetNext());
+            return tmp.GetValue();
+        }
+
+    private ListElement head;
+    }
+}
---
+++ C#/8/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var intQueue = new PriorityQueue<int>();
+            intQueue.Enqueue(5, 1);
+            intQueue.Enqueue(10, 0);
+            intQueue.Enqueue(5, 0);
+            while (true)
+            {
+                var val = 0;
+                try
+                {
+                    val = intQueue.Dequeue();
+                }
+                catch (QueueExeption e)
+                {
+                    break;
+                }
+                Console.WriteLine(val);
+            }
+            Console.Read();
+        }
+    }
+}
