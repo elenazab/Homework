d1c7a0fbd5d67cad9b7ef9e19786494b05c4fb2e
Elena <zlena@list.ru>
2012-12-15 01:57:02 +0400

    Теперь Точно:)

---
+++ C#/5/hw5t1/hw5t1/AbstractOperation.cs
@@ -0,0 +1,21 @@
+﻿
+namespace hw5t1
+{
+    public class AbstractOperation: TreeNode
+    {
+        public AbstractOperation()
+        {
+            Left = null;
+            Right = null;
+        }
+        public override double Calculate()
+        {
+            return 0;
+        }
+        public override void Print()
+        {
+        }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+    }
+}
---
+++ C#/5/hw5t1/hw5t1/TreeNode.cs
@@ -0,0 +1,8 @@
+﻿namespace hw5t1
+{
+    abstract public class TreeNode
+    {
+        abstract public double Calculate();
+        abstract public void Print();
+    }
+}
