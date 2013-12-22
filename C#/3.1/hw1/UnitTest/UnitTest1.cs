a6fe779edcc12ff29a5b02c5ee4c0828004345d7
Elena <zlena@list.ru>
2013-12-22 16:45:53 +0400

    тесты

---
+++ C#/3.1/hw1/UnitTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hw1t2;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            int n = 3;
+            int[,] array = {{1, 0, 1}, {0, 1, 0}, {1, 0, 1}};
+            Random testRnd = new Random(1);
+            Network testNetwork = new Network(n, array, testRnd);
+            Random testRnd2 = new Random(1);
+            for (int i = 0; i < n; i++)
+            {
+                if (testRnd2.Next(0, 2) < 1)
+                {
+                    Assert.IsTrue((testRnd2.Next(0, 100) > 65) == (testNetwork.getVirus(i)));
+                }
+                else
+                {
+                    Assert.IsTrue((testRnd2.Next(0, 100) > 55) == (testNetwork.getVirus(i)));
+                }
+            }
+        }
+    }
+}
