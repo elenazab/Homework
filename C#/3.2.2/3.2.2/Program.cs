15d9514dd7e8d6b04c83ca505e3eee340166557c
Elena <zlena@list.ru>
2013-12-22 17:05:17 +0400

    количество нулевых элементов в массиве

---
+++ C#/3.2.2/3.2.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace _3._2._2
+{
+    public class Program
+    {
+        /// <summary>
+        /// подсчет ненулевых элементов массива
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int Sum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (array[i] == 0)
+                    sum++;
+            }
+            return sum;
+        }
+
+        static void Main()
+        {
+            Random rnd = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < 10; i++)
+                array[i] = rnd.Next(-5, 5);
+            int sum = Sum(array);
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+    }
+}
+
---
+++ C#/3.2.2/UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3._2._2;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            int[] array1 = {1, 1, 0, 2, 1, 6, 7, 0, 0, 0};
+            Assert.IsTrue(Program.Sum(array1) == 4);
+            int[] array2 = { 1, 1, 5, 2, 1, 6, 7, 4, 3, 2 };
+            Assert.IsTrue(Program.Sum(array2) == 0);
+            int[] array3 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Assert.IsTrue(Program.Sum(array3) == 10);
+        }
+    }
+}
